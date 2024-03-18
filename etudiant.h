0e73023293395e67bf019d0ce66b28d797a8ebb6
Mohamed Bouafif <156509262+MohamedBouafif@users.noreply.github.com>
2024-03-18 21:47:16 +0100

    Update etudiant.h

--- etudiant.h
+++ etudiant.h
@@ -1,16 +1,19 @@
 #ifndef ETUDIANT_H_INCLUDED
 #define ETUDIANT_H_INCLUDED
-using namespace std;
 #include <iostream>
 #include <vector>
 #include <string>
 #pragma once
 #include "date.h"
+using namespace std;
+
+
 class etudiant
 {
+protected:
 
     string nom,prenom,mail,CIN,matricule_E;
-    float moyenne;
+    //float moyenne;
     bool travaille;
     vector<string> societes;
 
@@ -22,7 +25,7 @@ public:
 
     /** Const(s) & Dest**/
     etudiant(string="",string="",string="",string="",string="",bool =0,int=0 ,int =0,int =0,int=0,int=0 ,int=0 ,int=0,int =0,int=0 );
-    ~etudiant();
+    virtual ~etudiant();
     etudiant(const etudiant&);
 
 
@@ -34,7 +37,7 @@ public:
     string getmatricule_E(){return matricule_E;}
 
 
-    float getmoyenne(){return moyenne;}
+    //float getmoyenne(){return moyenne;}
 
 
     date getdate_de_naissance(){return date_de_naissance;}
@@ -50,14 +53,24 @@ public:
     void setmatricule_E(string matricule_E){this->matricule_E=matricule_E;}
     void setdate_de_naissance(date d){this->date_de_naissance.setjour(d.getjour());this->date_de_naissance.setmois(d.getmois());this->date_de_naissance.setannee(d.getannee());}
     void settravaille(bool travaille){this->travaille=travaille;}
-    void setmoyenne(float moyenne){this->moyenne=moyenne;}
+    //void setmoyenne(float moyenne){this->moyenne=moyenne;}
     void setdate_debut(date d){this->date_debut.setjour(d.getjour());this->date_debut.setmois(d.getmois());this->date_debut.setannee(d.getannee());}
     void setdate_fin(date d){this->date_fin.setjour(d.getjour());this->date_fin.setmois(d.getmois());this->date_fin.setannee(d.getannee());}
 
-/*************/
-    void afficher();
-    void saisir();
+
+    void chercher_Societe(string);
+
     int taille();
+
+    int dure_etude();//calculer la dure detude dans luniversite
+
+    void redouble();//afficher si letudiant a redoublé au moins une annee ou non
+
+/*************(CES METHODES SONT REDEFINIES)*********************/
+    virtual void afficher()=0;
+    virtual void saisir()=0;
+
+
 };
 
 
