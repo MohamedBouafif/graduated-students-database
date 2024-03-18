7a5f3270a290d32624c931f266f3668a48b94d9e
Mohamed Bouafif <156509262+MohamedBouafif@users.noreply.github.com>
2024-03-18 21:45:42 +0100

    Update main.cpp
    Testing all etudiant classes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
-//#include "date.cpp"
-//#include "etudiant.cpp"
-//#include"promotion.cpp"
-#include"pfe.cpp"
+#include "date.cpp"
+#include "etudiant.cpp"
+#include "etudinat_ing.cpp"
+#include "etudiant_master.cpp"
 int main()
 {
     /** test de la classe date: **/
-    /*
-    date d(10,12,2001);
+
+    /*date d(10,12,2001);
     d.afficher();
     d.setannee(2002);
     d.afficher();
@@ -19,25 +19,31 @@ int main()
 
 
     /**test de la classe etudiant**/
-    /*etudiant e("Mohamed","Bouafif","MohamedBouafif2020@gmail.com","14478956","EZF#54B",10.7);
+
+    /*etudiant e("Mohamed","Bouafif","MohamedBouafif2020@gmail.com","14478956","EZF#54B",10.7,12,05,2001,15,03,2022,15,03,2024);
     e.afficher();
     etudiant e2;
     e2.saisir();
     e2.afficher();
     date d = e.getdate_de_naissance();
     d.afficher();*/
+
+    /** test de classe etudiant ing**/
+    /*etudiant_ing e;
+    e.saisir();
+    e.afficher();
+    etudiant_ing e1(e);
+    e1.afficher();*/
+    //ALL good
+
+    /** test de classe etudiant master**/
+    etudiant_master e;
+    e.saisir();
+    e.afficher();
+    etudiant_master e1(e);
+    e1.afficher();
+    //ALL GOOD;
     
-     /**test de la classe promotion **/
-   /* promotion promo ;
-    promo.saisir_promo();
-    promo.afficher_promo();
-    etudiant  a;
-    a=promo.majeur_promo();
-    a.afficher();*/
-     /**test de la classe pfe **/
-    pfe pf ;
-    pf.saisir_pfe();
-    pf.afficher_pfe();
 
 
 }
