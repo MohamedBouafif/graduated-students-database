17269a50cf910677cb904b61109f89f6781d8777
Mohamed Bouafif <156509262+MohamedBouafif@users.noreply.github.com>
2024-03-18 21:48:57 +0100

    Create etudinat_ing.cpp

---
+++ etudinat_ing.cpp
@@ -0,0 +1,131 @@
+#include"etudinat_ing.h"
+/**Const**/
+etudiant_ing::etudiant_ing(string section,string nom,string prenom,string mail,string CIN,string matricule_E,bool travaille,int jn,int mn,int an,int jd,int md,int ad,int jf,int mf,int af):etudiant(nom,prenom,mail,CIN,matricule_E,travaille,jn,mn,an,jd,md,ad,jf,mf,af)
+{
+    this->section = section;
+}
+
+/**CONST PAR RECOPIE**/
+etudiant_ing::etudiant_ing(const etudiant_ing& e):etudiant(e)
+{
+    section = e.section;
+
+    for(int i = 0; i < (int) e.classes.size(); i++)
+    {
+        classes.push_back(e.classes[i]);
+    }
+    for(int i = 0;i < (int) e.moyennes.size(); i++)
+    {
+        moyennes.push_back(e.moyennes[i]);
+    }
+}
+
+/**DES**/
+etudiant_ing::~etudiant_ing()
+{
+   /* for(int i = 0; i < (int)moyennes.size(); i++)
+    {
+        delete moyennes[i];
+    }*/
+    moyennes.clear();
+/*
+    for(int i = 0;i < (int)classes.size(); i++)
+    {
+        delete classes[i];
+    }*/
+    classes.clear();
+}
+
+
+/**Les fonctions redefinies**/
+void etudiant_ing::afficher()
+{
+    etudiant::afficher();
+
+    cout<<"Section: "<<section<<endl;
+
+
+    cout<<"Affichage des classes durant ses annees detudes:\n";
+    for(int i = 0;i < (int)classes.size();i++)
+    {
+        cout<<classes[i]<<"\t";
+    }
+
+    cout<<endl;
+
+
+    cout<<"Affichage des moyennes durant ses annees detudes:\n";
+    for(int i = 0; i<(int)moyennes.size(); i++)
+    {
+        cout<<moyennes[i]<<"\t";
+    }
+
+    cout<<endl;
+}
+
+
+void etudiant_ing::saisir()
+{
+
+    etudiant::saisir();
+    cout<<"Saisir la section: "<<endl;
+    cin.ignore();
+    getline(cin,section);
+
+    int periode = dure_etude();
+
+    cout<<"Saisir les classes durant ces annes detudes\n";
+    for(int i = 0;i<periode;i++)
+    {
+        string c;
+        getline(cin,c);
+        classes.push_back(c);
+    }
+
+    cout<<"Saisir les moyennes durant ces annes detudes\n";
+    for(int i = 0; i < periode;i++)
+    {
+        float moy;
+        cin>>moy;
+        moyennes.push_back(moy);
+    }
+}
+
+
+/**NV FNC**/
+void etudiant_ing::afficher_classes()
+{
+    cout<<"Affichage des classes durant ses annees detudes:\n";
+    for(int i = 0;i < (int)classes.size();i++)
+    {
+        cout<<classes[i]<<"\t";
+    }
+
+    cout<<endl;
+}
+
+void etudiant_ing::afficher_moyennes()
+{
+
+    cout<<"Moyennes durant ses annees detudes:\n";
+    for(int i = 0; i<(int)moyennes.size(); i++)
+    {
+        cout<<moyennes[i]<<"\t";
+    }
+
+    cout<<endl;
+}
+
+void etudiant_ing::chercher_classe(string c)
+{
+    for(int i = 0; i <(int)classes.size(); i++)
+    {
+        if(classes[i]==c)
+        {
+            cout<<"Classe trouvee\n";
+            return;
+        }
+    }
+    cout<<"Classe non trouve!\n";
+    return ;
+}
