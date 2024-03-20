14233571e1f452b53a64ea605789b048ad04241a
souissiarij <153812559+souissiarij@users.noreply.github.com>
2024-03-20 23:08:12 +0100

    Add files via upload

---
+++ promotion_master.h
@@ -0,0 +1,26 @@
+#ifndef PROMOTION_MASTER_H_INCLUDED
+#define PROMOTION_MASTER_H_INCLUDED
+#include<vector>
+#include "date.cpp"
+#include "etudiant_master.cpp"
+class promotion_master
+{
+   int num_promo ;
+   date date_graduation_master ;
+   vector <etudiant_master> etudiants_master ;
+   int nb_etudiants_master ;
+   public :
+       promotion_master ();
+       promotion_master (date,int ,int);
+       ~promotion_master (){};
+       void saisir_promo_mast();
+       void afficher_promo_mast();
+       void afficher_promo_mast_triee();
+       etudiant_master majeur_promo_mast();
+
+
+
+};
+
+
+#endif // PROMOTION_MASTER_H_INCLUDED
