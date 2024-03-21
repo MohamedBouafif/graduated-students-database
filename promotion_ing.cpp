379dcfc1db876c8ac3c3efcecb6bffb7b4396b21
souissiarij <153812559+souissiarij@users.noreply.github.com>
2024-03-21 12:21:08 +0100

    Update promotion_ing.cpp

--- promotion_ing.cpp
+++ promotion_ing.cpp
@@ -1,15 +1,33 @@
 #include "promotion_ing.h"
-promotion_ing ::promotion_ing()
-{
-    num_promo =0 ;
-    nb_etudiants_ing=0;
-}
-promotion_ing :: promotion_ing(date date_graduation_ing, int num_promo, int nb_etudiants_ing)
+#include<algorithm>
+//constructeurs
+
+promotion_ing::promotion_ing (int num_promo,int nb_etudiants_ing,int jours,int mois ,int annee):
+   date_graduation_ing(jours,mois,annee)
 {
     this->date_graduation_ing=date_graduation_ing;
     this->num_promo=num_promo ;
     this->nb_etudiants_ing=nb_etudiants_ing;
 }
+//constructeur par recopie
+promotion_ing::pomotion_ing(const promotion_ing& p)
+{
+    num_promo = p.num_promo;
+    nb_etudiants_ing = p.nb_etudiants_ing;
+    date_graduation_ing = p.date_graduation_ing;
+    for (int i=0 ;i <p.etudiants_ing.size(); i++)
+    {
+        etudiants_ing[i]=p.etudiants_ing[i];
+    }
+}
+//destructeur
+promotion_ing::~promotion_ing()
+{
+    etudiants_ing.clear();
+}
+
+
+
 void promotion_ing :: saisir_promo_ing()
 {
     int rep =1 ;
@@ -43,29 +61,22 @@ void promotion_ing::afficher_promo_ing()
         etudiants_ing[i].afficher();
     }
 }
-void insertionSort(vector<etudiant_ing>& v) {
-
-    for (int i = 1; i < v.size() ; ++i)
-        {
-        etudiant_ing e ;
-        e=v[i];
-        int key = v[i].getmoyenne_general();
-        int j = i ;
-
-        while (j > 0 && v[j-1].getmoyenne_general() > key)
-            {
-            v[j ] = v[j-1];
-            j = j - 1;
-        }
-        v[j] = e;
-    }
+bool compare (etudiant_ing e1,etudiant_ing e2)
+{
+    return e1.getmoyenne_general() > e2.getmoyenne_general();
 }
 void promotion_ing::afficher_promo_ing_triee()
 {
-    insertionSort(etudiants_ing);
+    sort (etudiants_ing.begin(),etudiants_ing.end(),compare);
     afficher_promo_ing();
 
 }
+ etudiant_ing promotion_ing ::majeur_promo_ing()
+ {
+
+     return etudiants_ing[0];
+ }
+
  etudiant_ing promotion_ing ::majeur_promo_ing()
  {
      cout <<"**********affichage_majeur_promo_ing***************"<<endl;
