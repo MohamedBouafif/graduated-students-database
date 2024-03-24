fcd9eb83acf070ed227b8e6586dc608ac9cc59e6
Mohamed Bouafif <156509262+MohamedBouafif@users.noreply.github.com>
2024-03-24 23:32:59 +0100

    Update etudiants.h

--- etudiants.h
+++ etudiants.h
@@ -5,21 +5,32 @@
 class etudiants
 {
     friend class pfe;
-    friend class entreprise;
+    friend class entreprises;
+
     vector<etudiant*> tab;
 
 public:
 
+    vector<etudiant*> getab(){return tab;}
+
     etudiants();
     etudiants(const etudiants&);
     ~etudiants();
+
     void remplir();
     void afficher();
+
     int taille();
+
     void ajouter(etudiant_ing, int = 0);
     void ajouter(etudiant_master, int = 0);
     void ajouter(etudiant*,int = 0);
     void supprimer(int = 0);
+
+
+
+    friend ostream& operator<< (ostream&, etudiants&);
+    friend istream& operator>> (istream&, etudiants&);
 
 
 };
