96344cfb0de97963e9de59a0d466b7be6cb18d14
Mohamed Bouafif <156509262+MohamedBouafif@users.noreply.github.com>
2024-03-24 23:33:29 +0100

    Update etudiants.cpp

--- etudiants.cpp
+++ etudiants.cpp
@@ -13,8 +13,10 @@ etudiants::etudiants(const etudiants& w)
     {
         if(typeid(*w.tab[i])==typeid(etudiant_ing))
             e = new etudiant_ing(static_cast<const etudiant_ing&>(*w.tab[i]));
+
         else if(typeid(*w.tab[i])==typeid(etudiant_master))
             e = new etudiant_master(static_cast<const etudiant_master&>(*w.tab[i]));
+
         tab.push_back(e);
     }
 }
@@ -39,10 +41,10 @@ void etudiants::remplir()
     char rep;
     etudiant* e;
     int choix;
-    cout<<"Ajouter:\n";
+    cout<<"\nAjouter:\n";
     do
     {
-        cout<<"taper:\n1:etudiant ingenieur\t2:etudiant master\n";
+        cout<<"1:etudiant ingenieur\t2:etudiant master\n";
         cin>>choix;
         if(choix==1)
         {
@@ -55,7 +57,7 @@ void etudiants::remplir()
         else break;
         e->saisir();
         tab.push_back(e);
-        cout<<"Voulez-vous rajouter dautres etudiants?\n";
+        cout<<"Voulez-vous ajouter?\n";
         cout<<"1:Oui\t2:Non\n";
         cin>>rep;
     }
@@ -90,4 +92,54 @@ void etudiants::supprimer(int ind)
 {
     delete tab[ind];
     tab.erase(tab.begin()+ind);
+}
+
+int etudiants::taille()
+{
+    return tab.size();
+}
+
+
+
+
+
+ostream& operator<<(ostream& out,etudiants& etd)
+{
+
+    for(int i =0; i<(int)etd.tab.size(); i++)
+    {
+        etudiant* e = etd.tab[i];
+        out<<*e;
+    }
+    return out;
+}
+
+istream& operator>>(istream& in, etudiants& etd)
+{
+
+    char rep;
+    etudiant* e;
+    int choix;
+    cout<<"\nAjouter:\n";
+    do
+    {
+        cout<<"1:etudiant ingenieur\t2:etudiant master\n";
+        in>>choix;
+        if(choix==1)
+        {
+            e = new etudiant_ing();
+        }
+        else if(choix==2)
+        {
+            e = new etudiant_master();
+        }
+        else break;
+        in>>*e;
+        etd.tab.push_back(e);
+        cout<<"Voulez-vous ajouter?\n";
+        cout<<"1:Oui\t2:Non\n";
+        in>>rep;
+    }
+    while(rep=='1');
+    return in;
 }
