55fa49427867f44dffb2f89091faf535133ec351
souissiarij <153812559+souissiarij@users.noreply.github.com>
2024-04-13 18:06:27 +0100

    Update promotion_master.h

--- promotion_master.h
+++ promotion_master.h
@@ -2,17 +2,30 @@
 #define PROMOTION_MASTER_H_INCLUDED
 #include<vector>
 #include "date.cpp"
-#include "etudiant_master.cpp"
+#include "etudiant_master.h"
 class promotion_master
 {
-   int num_promo ;
-   date date_graduation_master ;
-   vector <etudiant_master> etudiants_master ;
-   int nb_etudiants_master ;
+   protected :
+       int num_promo_master ;
+       date date_graduation_master ;
+       vector <etudiant_master> etudiants_master ;
+       int nb_etudiants_master ;
    public :
        promotion_master (int=0,int=0,int=0,int=0,int=0);
        promotion_master (const promotion_master& );
        ~promotion_master ();
+       //setteurs et getteurs
+       int get_num_promo_master (){return num_promo_master;}
+       int get_nb_etudiants(){return nb_etudiants_master;}
+       date get_date_graduation_master(){return date_graduation_master;}
+       etudiant_master get_etudiant(int i){return etudiants_master[i];}
+
+       void set_num_promo (int num_promo_master){this->num_promo_master=num_promo_master;}
+       void set_nb_etudiants(int nb_etudiants_master) {this->nb_etudiants_master=nb_etudiants_master;}
+       void set_date_graduation_master (date date_graduation_master ){this->date_graduation_master=date_graduation_master;}
+
+
+       void ajouter_etudiant_master(etudiant_master );
        void saisir_promo_mast();
        void afficher_promo_mast();
        void afficher_promo_mast_triee();
@@ -20,8 +33,13 @@ class promotion_master
 
 
 
-};
+       friend ostream& operator<<(ostream&,promotion_master&);
+       friend istream& operator>>(istream&,promotion_master&);
 
+
+
+
+};
 
 
 #endif // PROMOTION_MASTER_H_INCLUDED
