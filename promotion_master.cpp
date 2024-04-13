73a0de65af9fcfc24a4d1d41a4fa40a3bd554e9b
souissiarij <153812559+souissiarij@users.noreply.github.com>
2024-04-13 18:05:56 +0100

    Update promotion_master.cpp

--- promotion_master.cpp
+++ promotion_master.cpp
@@ -1,18 +1,18 @@
 #include "promotion_master.h"
 #include<algorithm>
-//constructeurs
+//constructeur
 
-promotion_master::promotion_master (int num_promo,int nb_etudiants_master,int jours,int mois ,int annee):
+promotion_master::promotion_master (int num_promo_master,int nb_etudiants_master,int jours,int mois ,int annee):
    date_graduation_master(jours,mois,annee)
 {
     this->date_graduation_master=date_graduation_master;
-    this->num_promo=num_promo ;
+    this->num_promo_master=num_promo_master ;
     this->nb_etudiants_master=nb_etudiants_master;
 }
 //constructeur par recopie
 promotion_master ::promotion_master (const promotion_master& p)
 {
-    num_promo = p.num_promo;
+    num_promo_master = p.num_promo_master;
     nb_etudiants_master = p.nb_etudiants_master;
     date_graduation_master = p.date_graduation_master;
     for (int i=0 ;i <p.etudiants_master.size(); i++)
@@ -25,11 +25,12 @@ promotion_master::~promotion_master()
      {
          etudiants_master.clear();
      }
+//fonction saisie
 void promotion_master :: saisir_promo_mast()
 {
     int rep =1 ;
     cout << "entrer le numero de promotion: "<<endl ;
-    cin >>num_promo;
+    cin >>num_promo_master;
     cout << "entrer la  date de graduation de cette promo: "<< endl ;
     date_graduation_master.saisir();
     cout <<"saisie des etudiants de cette promotion : "<<endl;
@@ -51,8 +52,15 @@ void promotion_master :: saisir_promo_mast()
     }
 
 void promotion_master::afficher_promo_mast()
-{   nb_etudiants_master= etudiants_master.size();
-    cout <<"***************AFFICHAGE PROMOTION MASTER*************************"<<endl ;
+{
+
+    cout <<"***************AFFICHAGE PROMOTION MASTER *************************"<<endl ;
+    cout <<"numero de promotion  : "<<num_promo_master<<endl ;
+    cout <<"date de graduation : ";
+    date_graduation_master.afficher() ;
+    cout <<"nombres  d'etudiants: "<< nb_etudiants_master<<endl ;
+    nb_etudiants_master= etudiants_master.size();
+
     for (int i=0 ; i<nb_etudiants_master;i++)
     {
         etudiants_master[i].afficher();
@@ -75,7 +83,7 @@ void promotion_master::afficher_promo_mast()
         v[j] = e;
     }
 }*/
-
+// fonction de tri de vecteurs des etudiants
 bool compare (etudiant_master e1,etudiant_master e2)
 {
     return e1.getmoyenne_general() > e2.getmoyenne_general();
@@ -86,8 +94,55 @@ void promotion_master::afficher_promo_mast_triee()
     afficher_promo_mast();
 
 }
+//retourner le majeur de promo
  etudiant_master promotion_master ::majeur_promo_mast()
  {
 
      return etudiants_master[0];
+ }
+void promotion_master::ajouter_etudiant_master(etudiant_master e )
+{
+    etudiants_master.push_back(e);
+}
+
+
+  ostream& operator<<(ostream& out,promotion_master& p)
+  {
+       out <<"***************AFFICHAGE PROMOTION MASTER *************************"<<endl ;
+    out <<"numero de promotion  : "<<p.num_promo_master<<endl ;
+    out <<"date de graduation : ";
+    p.date_graduation_master.afficher() ;
+    out <<"nombres  d'etudiants: "<< p.nb_etudiants_master<<endl ;
+    p.nb_etudiants_master= p.etudiants_master.size();
+
+    for (int i=0 ; i<p.nb_etudiants_master;i++)
+    {
+        p.etudiants_master[i].afficher();
+    }
+    return out;
+  }
+ istream& operator>>(istream& in ,promotion_master& p)
+ {
+     int rep =1 ;
+    cout << "entrer le numero de promotion: "<<endl ;
+    in >>p.num_promo_master;
+    cout << "entrer la  date de graduation de cette promo: "<< endl ;
+    p.date_graduation_master.saisir();
+    cout <<"saisie des etudiants de cette promotion : "<<endl;
+    etudiant_master e ;
+    e.saisir();
+    p.etudiants_master.push_back(e);
+    cout<<"Voulez-vous rajouter dautres etudiants?\n";
+    cout<<"1:Oui\t2:Non\n";
+    in>>rep;
+    while( rep ==1)
+    {
+    etudiant_master e ;
+    e.saisir();
+    p.etudiants_master.push_back(e);
+    cout<<"Voulez-vous rajouter dautres etudiants?\n";
+    cout<<"1:Oui\t2:Non\n";
+    in>>rep;
+    }
+    return in;
  }
