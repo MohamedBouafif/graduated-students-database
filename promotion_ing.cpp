a3933ea460a7932b481a8954a60cd4a5b1b0c2ff
souissiarij <153812559+souissiarij@users.noreply.github.com>
2024-04-13 18:04:25 +0100

    Update promotion_ing.cpp

--- promotion_ing.cpp
+++ promotion_ing.cpp
@@ -1,18 +1,18 @@
 #include "promotion_ing.h"
-#include<algorithm>
+
 //constructeurs
 
-promotion_ing::promotion_ing (int num_promo,int nb_etudiants_ing,int jours,int mois ,int annee):
+promotion_ing::promotion_ing (int num_promo_ing,int nb_etudiants_ing,int jours,int mois ,int annee):
    date_graduation_ing(jours,mois,annee)
 {
     this->date_graduation_ing=date_graduation_ing;
-    this->num_promo=num_promo ;
+    this->num_promo_ing=num_promo_ing ;
     this->nb_etudiants_ing=nb_etudiants_ing;
 }
 //constructeur par recopie
 promotion_ing::pomotion_ing(const promotion_ing& p)
 {
-    num_promo = p.num_promo;
+    num_promo_ing = p.num_promo_ing;
     nb_etudiants_ing = p.nb_etudiants_ing;
     date_graduation_ing = p.date_graduation_ing;
     for (int i=0 ;i <p.etudiants_ing.size(); i++)
@@ -32,7 +32,7 @@ void promotion_ing :: saisir_promo_ing()
 {
     int rep =1 ;
     cout << "entrer le numero de promotion: "<<endl ;
-    cin >>num_promo;
+    cin >>num_promo_ing;
     cout << "entrer la  date de graduation de cette promo: "<< endl ;
     date_graduation_ing.saisir();
     cout <<"saisie des etudiants de cette promotion : "<<endl;
@@ -54,8 +54,14 @@ void promotion_ing :: saisir_promo_ing()
     }
 
 void promotion_ing::afficher_promo_ing()
+
 {   nb_etudiants_ing= etudiants_ing.size();
     cout <<"***************AFFICHAGE PROMOTION INGENIEURS *************************"<<endl ;
+    cout <<"numero de promotion d'ingenieur : "<<num_promo_ing<<endl ;
+    cout <<"date de graduation : ";
+    date_graduation_ing.afficher() ;
+    cout <<"nombres  d'etudiants: "<< nb_etudiants_ing<<endl ;
+
     for (int i=0 ; i<nb_etudiants_ing;i++)
     {
         etudiants_ing[i].afficher();
@@ -66,9 +72,11 @@ bool compare (etudiant_ing e1,etudiant_ing e2)
     return e1.getmoyenne_general() > e2.getmoyenne_general();
 }
 void promotion_ing::afficher_promo_ing_triee()
-{
-    sort (etudiants_ing.begin(),etudiants_ing.end(),compare);
+{    //Sort the vector using the compare function
+//   sort (etudiants_ing.begin(),etudiants_ing.end(),compare);
+    //display the sorted result
     afficher_promo_ing();
+
 
 }
  etudiant_ing promotion_ing ::majeur_promo_ing()
@@ -76,9 +84,52 @@ void promotion_ing::afficher_promo_ing_triee()
 
      return etudiants_ing[0];
  }
+ void promotion_ing::ajouter_etudiant_ing(etudiant_ing e )
+{
+    etudiants_ing.push_back(e);
+}
 
- etudiant_ing promotion_ing ::majeur_promo_ing()
- {
-     cout <<"**********affichage_majeur_promo_ing***************"<<endl;
-     return etudiants_ing[(etudiants_ing.size()-1)];
- }
+ostream& operator<<(ostream& out,promotion_ing& p)
+{
+    p.nb_etudiants_ing= p.etudiants_ing.size();
+    out <<"***************AFFICHAGE PROMOTION INGENIEURS *************************"<<endl ;
+    out <<"numero de promotion d'ingenieur : "<< p.num_promo_ing<<endl ;
+    out <<"date de graduation : ";
+    p.date_graduation_ing.afficher() ;
+    out <<"nombres  d'etudiants: "<< p.nb_etudiants_ing<<endl ;
+
+    for (int i=0 ; i<p.nb_etudiants_ing;i++)
+    {
+        p.etudiants_ing[i].afficher();
+    }
+    return out ;
+
+}
+istream& operator>>(istream& in,promotion_ing& p)
+{
+    int rep =1 ;
+    cout << "entrer le numero de promotion: "<<endl ;
+    in >>p.num_promo_ing;
+    cout << "entrer la  date de graduation de cette promo: "<< endl ;
+    p.date_graduation_ing.saisir();
+    cout <<"saisie des etudiants de cette promotion : "<<endl;
+    etudiant_ing e ;
+    e.saisir();
+    p.etudiants_ing.push_back(e);
+    cout<<"Voulez-vous rajouter dautres etudiants?\n";
+    cout<<"1:Oui\t2:Non\n";
+    in>>rep;
+    while( rep ==1)
+    {
+    etudiant_ing e ;
+    e.saisir();
+    p.etudiants_ing.push_back(e);
+    cout<<"Voulez-vous rajouter dautres etudiants?\n";
+    cout<<"1:Oui\t2:Non\n";
+    in>>rep;
+    }
+
+    return in;
+}
+
+
