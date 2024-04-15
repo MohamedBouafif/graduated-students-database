9c275dd8db5e18742ea19452245ec6e92cd0a292
Mohamed Bouafif <156509262+MohamedBouafif@users.noreply.github.com>
2024-04-15 19:02:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
+#include "date.cpp"
 #include "etudiant.cpp"
-//#include "etudiant_ing.cpp"
-#include "etudiants.cpp"
+#include "etudinat_ing.cpp"
 #include "etudiant_master.cpp"
+#include "etudiants.cpp"
 #include "pfe.cpp"
-//#include "promotion_master.cpp"
-#include "promotion_ing.cpp"
+
+//#include "pfes.cpp"
+//#include "entreprise.cpp"
 int main()
 {
     /** test de la classe date: **/
@@ -18,18 +20,51 @@ int main()
     date d1;
     d1.saisir();
     d1.afficher();*/
+        /**test de surcharge de la classe date**/
+            /*date d1;
+            cin>>d1;
+            cout<<d1;
+            date d2 =d1;
+            cout<<d2;
+            if(d2==d1)cout<<"ok";
+            date d3;
+            cin>>d3;
+            vector<date> v(3);
+            v[0]=d1;
+            v[1]=d2;
+            v[2] = d3;
+            for(int i = 0; i < v.size(); i++) cout<<v[i];
+            sort(v.begin(),v.end());
+            cout<<endl;
+            for(int i = 0; i < v.size(); i++) cout<<v[i];*/
+            //ALL GOOD
 
 
 
     /**test de la classe etudiant**/
 
-    /*etudiant e("Mohamed","Bouafif","MohamedBouafif2020@gmail.com","14478956","EZF#54B",10.7,12,05,2001,15,03,2022,15,03,2024);
+    /*etudiant e;
     e.afficher();
     etudiant e2;
     e2.saisir();
     e2.afficher();
     date d = e.getdate_de_naissance();
     d.afficher();*/
+            /**test de surcharge de la classe etudiant**/
+            /*etudiant e1;
+            etudiant e2;
+            cin>>e1;
+            cout<<endl;
+            cin>>e2;
+            cout<<endl;
+            if(e1==e2)cout<<"y\n";else cout<<"no\n";
+            etudiant e3 = e2;
+            cout<<e3;*/
+            //ALL GOOD
+
+
+
+
 
     /** test de classe etudiant ing**/
     /*etudiant_ing e;
@@ -38,6 +73,9 @@ int main()
     etudiant_ing e1(e);
     e1.afficher();*/
     //ALL good
+
+
+
 
     /** test de classe etudiant master**/
     /*etudiant_master e;
@@ -48,38 +86,36 @@ int main()
     //ALL GOOD;
 
     /**TEST DE classe etudiantS **/
-    /*etudiants e;
-    e.remplir();
-    e.afficher();
-    //ALL GOOD;*/
 
-    
+        /**surcharge <<  et   >>  **/
+            /*etudiants e;
+            cin>>e;
+            cout<<e;*/
+
+    /*e.remplir();
+    e.afficher();*/
+    //ALL GOOOD!
+
     /**Test de classe pfe**/
-   /* pfe p;
+    /*pfe p(e);
+    p.afficher();
     p.saisir();
     p.afficher();
     pfe p1(p);//test de constructeur par recopie
     p1.afficher();
 
-    etudiants e = p1.getetudiants(); //test de getteur des etudiants qui ont travaille dans une pfe donné
-    e.afficher();
-    //ALL GOOD ! */
-       /**Test de classe promotion_mast **/
-      /* promotion_master p ;
-       p.saisir_promo_mast() ;
-       p.afficher_promo_mast_triee() ;
-       etudiant_master e ;
-       e = p.majeur_promo_mast();
-       e.afficher();*/
+    etudiants e1 = p1.getetudiants(); //test de getteur des etudiants qui ont travaille dans une pfe donné
+    e1.afficher();*/
+    //ALL GOOD !
 
+    /** Test classe pfes**/
+    /*pfes p;
+    p.remplir();
+    p.afficher();
+    //ALL GOOD!
 
-       /**Test de classe promotion_ing+ **/
-       promotion_ing p ;
-       p.saisir_promo_ing() ;
-       p.afficher_promo_ing_triee() ;
-       etudiant_ing e ;
-     cout<<"***************affichage majeur promo ****************"<<endl ; 
-       e = p.majeur_promo_ing ();
-       e.afficher();
+    entreprises en;
+    en.remplir(e,p);
+    en.afficher_par_etudiant();*/
 
 }
