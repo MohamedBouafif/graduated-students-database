47d3cd8f1ddad4ddd84d51c63c3edcdbe184798d
Mohamed Bouafif <156509262+MohamedBouafif@users.noreply.github.com>
2024-04-17 21:04:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #include "etudiants.cpp"
 #include "pfe.cpp"
 
-//#include "pfes.cpp"
+#include "pfes.cpp"
 //#include "entreprise.cpp"
 int main()
 {
@@ -59,8 +59,10 @@ int main()
             cout<<endl;
             if(e1==e2)cout<<"y\n";else cout<<"no\n";
             etudiant e3 = e2;
-            cout<<e3;*/
-            //ALL GOOD
+            cout<<e3<<endl;
+            e3 = e1;
+            cout<<e3;    // la partie dynamique est bien modifié
+            //ALL GOOD*/
 
 
 
@@ -72,6 +74,19 @@ int main()
     e.afficher();
     etudiant_ing e1(e);
     e1.afficher();*/
+            /**Test de surcharge de classe etudiant ing**/
+                /*etudiant_ing e;
+                cin>>e;
+                cout<<e;
+                etudiant_ing e2;
+                cin>>e2;
+                //e2=e; //la partie dynamique est bien modifié
+                cout<<e2;
+                vector<etudiant_ing> v(2);
+                v[0] = e;
+                v[1] = e2;
+                sort(v.begin(),v.end());
+                cout<<v[0]<<endl<<v[1];*/
     //ALL good
 
 
@@ -83,38 +98,74 @@ int main()
     e.afficher();
     etudiant_master e1(e);
     e1.afficher();*/
+        /**Test de surcharge de classe etudiant master**/
+            /*etudiant_master e;
+            cin>>e;
+            etudiant_master e1;
+            cin>>e1;
+            cout<<"e : \n";
+            cout<<e<<endl<<"e1 : \n"<<e1<<endl;
+            e1 = e;
+            cout<<e1;*/
     //ALL GOOD;
 
+
+
     /**TEST DE classe etudiantS **/
-
-        /**surcharge <<  et   >>  **/
-            /*etudiants e;
-            cin>>e;
-            cout<<e;*/
-
     /*e.remplir();
     e.afficher();*/
+        /**Test de surcharge de classe etudiants**/
+            /*etudiants e;
+            cin>>e;
+            cout<<e<<endl;
+            etudiants e1;
+            cin>>e1;
+            //e1= e;
+            //cout<<e1;
+            e = e+e1;
+            cout<<e;
+
+            etudiants e;
+            cin>>e;
+            e.afficher_tri_croissant();//TRI marche ok*/
     //ALL GOOOD!
 
+
     /**Test de classe pfe**/
-    /*pfe p(e);
-    p.afficher();
+    /*etudiants e;
+    cin>>e;
+    pfe p(e);
     p.saisir();
-    p.afficher();
+    cout<<"pfe p=\n"<<p<<endl;
+    pfe g;
+    cin>>g;
+
     pfe p1(p);//test de constructeur par recopie
     p1.afficher();
+    p.saisir(e);
+    cout<<g;*/
 
-    etudiants e1 = p1.getetudiants(); //test de getteur des etudiants qui ont travaille dans une pfe donné
-    e1.afficher();*/
+    /*etudiants e1 = p1.getetudiants(); //test de getteur des etudiants qui ont travaille dans une pfe donné
+    cout<<e1*/
+    /*pfe e;
+    cin>>e;
+    cout<<e<<endl;*/
     //ALL GOOD !
 
     /** Test classe pfes**/
     /*pfes p;
     p.remplir();
-    p.afficher();
-    //ALL GOOD!
+    p.afficher();    -> creation de pfes sans etudiants marche ok !
+    //ALL GOOD!*/
+    /*etudiants e;
+    cin>>e;
+    pfes p;
+    p.remplir(e);
+    //Le programme sarrette ici !!!!
+    cout<<endl<<p.taille()<<endl;
+    p.afficher();*/
 
-    entreprises en;
+    /*entreprises en;
     en.remplir(e,p);
     en.afficher_par_etudiant();*/
 
