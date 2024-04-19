7b07b5fcf2317b86a842e45c6319eb3fb31adbc9
Mohamed Bouafif <156509262+MohamedBouafif@users.noreply.github.com>
2024-04-19 22:09:01 +0100

    Update etudiants.h

--- etudiants.h
+++ etudiants.h
@@ -1,36 +1,61 @@
 #ifndef ETUDIANTS_H_INCLUDED
 #define ETUDIANTS_H_INCLUDED
 #pragma once
+
+
 #include "etudiant.h"
+#include "etudiant_master.h"
+#include "etudinat_ing.h"
+
+
 class etudiants
 {
     friend class pfe;
     friend class entreprises;
 
+
     vector<etudiant*> tab;
 
 public:
 
-    vector<etudiant*> getab(){return tab;}
+/**Const(s) & Dest**/
+
 
     etudiants();
     etudiants(const etudiants&);
     ~etudiants();
 
-    void remplir();
-    void afficher();
+/**Get & set**/
+    vector<etudiant*> gettab(){return tab;}
 
-    int taille();
+    int taille(){return tab.size();}
+
 
     void ajouter(etudiant_ing, int = 0);
     void ajouter(etudiant_master, int = 0);
     void ajouter(etudiant*,int = 0);
-    void supprimer(int = 0);
+    void supprimer(int = 0); //indice
+
+
+/*************(CES METHODES SONT REDEFINIES~surchagés)*********************/
+    void remplir();
+    void afficher();
+
+
+/********************************************/
 
 
 
-    friend ostream& operator<< (ostream&, etudiants&);
-    friend istream& operator>> (istream&, etudiants&);
+/**************Surcharge des operateurs**************************/
+    friend ostream& operator << (ostream&, etudiants&);
+    friend istream& operator >> (istream&, etudiants&);
+    etudiants& operator = (const etudiants&);
+    etudiants& operator + (const etudiants&);
+
+    void afficher_tri_croissant();
+    float pourcentage_reussite();
+    float pourcentage_redouble();
+    float pourcentage_chommage();
 
 
 };
